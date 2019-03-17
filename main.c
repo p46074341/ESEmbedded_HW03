56224d54bfab7e594c4f3a0db0cc9afe597aa5b4
p46074341 <p46074341@mail.ncku.edu.tw>
2019-03-17 14:34:50 +0800

    modify code and comment

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 void reset_handler(void)
-{
+{	
 	while (1)
+	function1(1, 2, 3, 4);
+	caller1();
 		;
 }
+
+int function1(int a,int b, int c, int d)
+{
+	return a+b+c-d;
+}
+
+int caller1(void)
+{
+	return function1(1, 2, 3, 4);
+}
+
